56721d45284ea7789350f5af865beb0f499b6b8b
Erick_Prado404 <73625446+ErickPrado101@users.noreply.github.com>
2023-12-20 21:17:30 -0300

    Update exercicio1.js

--- exercicio1.js
+++ exercicio1.js
@@ -1,14 +1,26 @@
-function isPrime(number) {
-  if (number <= 1) {
+// Função para verificar se um número é primo
+const isPrime = (num) => {
+  if (num <= 1) {
     return false;
   }
 
-  const divisors = Array.from({ length: Math.floor(Math.sqrt(number)) - 1 }, (_, index) => index + 2);
+  const sieve = Array(num + 1).fill(true);
+  sieve[0] = sieve[1] = false;
 
-  return divisors.every(divisor => number % divisor !== 0);
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (sieve[i]) {
+      for (let j = i * i; j <= num; j += i) {
+        sieve[j] = false;
+      }
+    }
+  }
+
+  return sieve[num];
+};
+
+const numeroTeste = 17;
+if (isPrime(numeroTeste)) {
+  console.log(`${numeroTeste} é um número primo.`);
+} else {
+  console.log(`${numeroTeste} não é um número primo.`);
 }
-
-console.log(isPrime(2));   
-console.log(isPrime(3));   
-console.log(isPrime(4));   
-console.log(isPrime(13));  
